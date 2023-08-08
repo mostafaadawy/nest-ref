28881da86fbff6410a9b60a67ac748e692f3a1c8
Mostafa Sayed Adawy <mostafa_adawy@ymail.com>
2023-08-08 13:05:06 +0300

    exporting request

--- README.md
+++ README.md
@@ -206,3 +206,16 @@ ERROR [ExceptionHandler] Nest can't resolve dependencies of the AuthService (?).
 
 - instead of importing it indedidually we can use `@Global()` decoration on the required module which is prisma module to allow these modules to other modules by importing it only in the main module and set it as global
 - now you can remove its import from other indevidual module as long as it is seen and imported from the main module and it is set to be global module
+
+# Engough for configuration lets logic programming by writing our signing up and loging function
+
+- we can lof the request as follows
+
+```
+signup(@Req() request: Request) {
+    console.log('request', request);
+    return this.authService.signUp();
+  }
+```
+
+-
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,23 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Req,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService){}
-    @Post('signup')
-    signup(){
-       return this.authService.signUp()
-    }
-    @Post('signin')
-    signin(){
-       return this.authService.login();
-    }
+  constructor(
+    private readonly authService: AuthService,
+  ) {}
+  @Post('signup')
+  signup(@Req() request: Request) {
+    console.log('request', request);
+    return this.authService.signUp();
+  }
+  @Post('signin')
+  signin() {
+    return this.authService.login();
+  }
 }
